167a522a1dd8355c6457eb384bca157f2c5e0f24
David Carver <dcarver@intalio.com>
2011-06-28 15:06:29 -0400

    TURMERIC-1466: Add TypeLibrary configuration tests and fix issue in lifecycle-mapping-metadata.xml file that was looking for the wrong configurator when executing the turmeric-maven-plugin.

--- test/org.ebayopensource.turmeric.eclipse.maven.sconfig.tests/src/org/ebayopensource/turmeric/eclipse/maven/sconfig/tests/TurmericStandardProjectConfiguratorTest.java
+++ test/org.ebayopensource.turmeric.eclipse.maven.sconfig.tests/src/org/ebayopensource/turmeric/eclipse/maven/sconfig/tests/TurmericStandardProjectConfiguratorTest.java
@@ -1,104 +1,93 @@
 package org.ebayopensource.turmeric.eclipse.maven.sconfig.tests;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.io.IOException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.core.ClasspathEntry;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TurmericStandardProjectConfiguratorTest extends
 		AbstractMavenProjectTestCase {
-	@Test
-	public void testJAXBEpisodeCodegenToolsProject() throws Exception {
+
+	private IProject buildProject(String testProjectPOMPath)
+			throws IOException, CoreException, InterruptedException {
 		ResolverConfiguration configuration = new ResolverConfiguration();
-		IProject project1 = importProject("projects/codegen-tools/pom.xml",
-				configuration);
+		IProject project1 = importProject(testProjectPOMPath, configuration);
 		waitForJobsToComplete();
 
 		project1.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
 		project1.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
 		waitForJobsToComplete();
+		return project1;
+	}
 
-//		assertNoErrors(project1);
+	private void assertSourcePathFound(IClasspathEntry[] cp1, Path sourcePath) {
+		boolean fndsw = false;
+		for (IClasspathEntry entry : cp1) {
+			if (entry.getPath().equals(sourcePath)) {
+				fndsw = true;
+				break;
+			}
+		}
+		assertTrue("Missing " + sourcePath.toString() + "source entry", fndsw);
+	}
 
+	private IClasspathEntry[] getClasspath(IProject project1)
+			throws JavaModelException {
 		IJavaProject javaProject1 = JavaCore.create(project1);
 		IClasspathEntry[] cp1 = javaProject1.getRawClasspath();
-		
-		boolean fndsw = false;
-		Path sourcePath = new Path("/codegen-tools/target/generated-sources/jaxb-episode");
-		for (IClasspathEntry entry : cp1) {
-			
-			if (entry.getPath().equals(sourcePath)) {
-				fndsw = true;
-				break;
-			}
-		}
-		
-		assertTrue("Missing generated-sources/jaxb-episode source entry", fndsw);
-
-		fndsw = false;
-		sourcePath = new Path("/codegen-tools/target/generated-resources/jaxb-episode");
-		for (IClasspathEntry entry : cp1) {
-			
-			if (entry.getPath().equals(sourcePath)) {
-				fndsw = true;
-				break;
-			}
-		}
-
-		assertTrue("Missing generated-resources/jaxb-episode source entry", fndsw);
-
-		
-//		assertEquals(new Path(
-//				"/codegen-tools/target/generated-sources/jaxb-episode"),
-//				cp1[5].getPath());
-
-		assertTrue( project1.getFile(
-		  "target/generated-sources/jaxb-episode/org/ebayopensource/turmeric/runtime/codegen/common/CodeGenOptionType.java"
-		 ).isSynchronized( IResource.DEPTH_ZERO ) );
-		assertTrue( project1.getFile(
-		  "target/generated-sources/jaxb-episode/org/ebayopensource/turmeric/runtime/codegen/common/CodeGenOptionType.java"
-		 ).isAccessible() );
+		return cp1;
 	}
 
 	@Test
-	@Ignore
-	public void test_p001_simple_v3() throws Exception {
-		ResolverConfiguration configuration = new ResolverConfiguration();
-		IProject project1 = importProject(
-				"projects/antlr/antlr-v3-p001/pom.xml", configuration);
-		waitForJobsToComplete();
+	public void testJAXBEpisodeCodegenToolsProject() throws Exception {
+		String testProjectPOMPath = "projects/codegen-tools/pom.xml";
+		IProject project = buildProject(testProjectPOMPath);
 
-		project1.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
-		project1.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
-		waitForJobsToComplete();
+		// assertNoErrors(project1);
 
-		assertNoErrors(project1);
+		IClasspathEntry[] cp1 = getClasspath(project);
 
-		IJavaProject javaProject1 = JavaCore.create(project1);
-		IClasspathEntry[] cp1 = javaProject1.getRawClasspath();
+		Path sourcePath = new Path(
+				"/codegen-tools/target/generated-sources/jaxb-episode");
+		assertSourcePathFound(cp1, sourcePath);
 
-		assertEquals(new Path("/antlr-v3-p001/target/generated-sources/antlr"),
-				cp1[3].getPath());
+		sourcePath = new Path(
+				"/codegen-tools/target/generated-resources/jaxb-episode");
+		assertSourcePathFound(cp1, sourcePath);
 
-		assertTrue(project1.getFile(
-				"target/generated-sources/antlr/test/SampleParser.java")
+		String file = "target/generated-sources/jaxb-episode/org/ebayopensource/turmeric/runtime/codegen/common/CodeGenOptionType.java";
+		assertTrue(project.getFile(file).isSynchronized(IResource.DEPTH_ZERO));
+		assertTrue(project.getFile(file).isAccessible());
+	}
+
+	@Test
+	public void testTypeLibraryProject() throws Exception {
+		String testProjectPOMPath = "projects/common-type-library/pom.xml";
+		IProject project = buildProject(testProjectPOMPath);
+		IClasspathEntry[] cp1 = getClasspath(project);
+
+		Path sourcePath = new Path(
+				"/common-type-library/target/generated-sources/codegen");
+		assertSourcePathFound(cp1, sourcePath);
+
+		sourcePath = new Path(
+				"/common-type-library/target/generated-resources/codegen");
+		assertSourcePathFound(cp1, sourcePath);
+
+		String file = "target/generated-sources/codegen/org/ebayopensource/turmeric/common/v1/types/AckValue.java";
+		assertTrue("File is not synchronized:  " + file, project.getFile(file)
 				.isSynchronized(IResource.DEPTH_ZERO));
-		assertTrue(project1.getFile(
-				"target/generated-sources/antlr/test/SampleParser.java")
-				.isAccessible());
+		assertTrue("File is not accessible: " + file, project.getFile(file).isAccessible());
 	}
 
 }
