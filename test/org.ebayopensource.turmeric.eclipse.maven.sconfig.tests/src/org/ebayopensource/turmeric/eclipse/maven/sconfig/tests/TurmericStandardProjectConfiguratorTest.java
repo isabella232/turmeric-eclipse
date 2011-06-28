2ad0c157920c3fb92e6c50b549f26dcecea3786f
David Carver <dcarver@intalio.com>
2011-06-28 15:37:32 -0400

    TURMERIC-1466: add error-library configuration check.

--- test/org.ebayopensource.turmeric.eclipse.maven.sconfig.tests/src/org/ebayopensource/turmeric/eclipse/maven/sconfig/tests/TurmericStandardProjectConfiguratorTest.java
+++ test/org.ebayopensource.turmeric.eclipse.maven.sconfig.tests/src/org/ebayopensource/turmeric/eclipse/maven/sconfig/tests/TurmericStandardProjectConfiguratorTest.java
@@ -21,18 +21,18 @@ public class TurmericStandardProjectConfiguratorTest extends
 	private IProject buildProject(String testProjectPOMPath)
 			throws IOException, CoreException, InterruptedException {
 		ResolverConfiguration configuration = new ResolverConfiguration();
-		IProject project1 = importProject(testProjectPOMPath, configuration);
+		IProject project = importProject(testProjectPOMPath, configuration);
 		waitForJobsToComplete();
 
-		project1.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
-		project1.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+		project.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+		project.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
 		waitForJobsToComplete();
-		return project1;
+		return project;
 	}
 
-	private void assertSourcePathFound(IClasspathEntry[] cp1, Path sourcePath) {
+	private void assertSourcePathFound(IClasspathEntry[] cp, Path sourcePath) {
 		boolean fndsw = false;
-		for (IClasspathEntry entry : cp1) {
+		for (IClasspathEntry entry : cp) {
 			if (entry.getPath().equals(sourcePath)) {
 				fndsw = true;
 				break;
@@ -41,11 +41,11 @@ public class TurmericStandardProjectConfiguratorTest extends
 		assertTrue("Missing " + sourcePath.toString() + "source entry", fndsw);
 	}
 
-	private IClasspathEntry[] getClasspath(IProject project1)
+	private IClasspathEntry[] getClasspath(IProject project)
 			throws JavaModelException {
-		IJavaProject javaProject1 = JavaCore.create(project1);
-		IClasspathEntry[] cp1 = javaProject1.getRawClasspath();
-		return cp1;
+		IJavaProject javaProject1 = JavaCore.create(project);
+		IClasspathEntry[] cp = javaProject1.getRawClasspath();
+		return cp;
 	}
 
 	@Test
@@ -55,15 +55,15 @@ public class TurmericStandardProjectConfiguratorTest extends
 
 		// assertNoErrors(project1);
 
-		IClasspathEntry[] cp1 = getClasspath(project);
+		IClasspathEntry[] cp = getClasspath(project);
 
 		Path sourcePath = new Path(
 				"/codegen-tools/target/generated-sources/jaxb-episode");
-		assertSourcePathFound(cp1, sourcePath);
+		assertSourcePathFound(cp, sourcePath);
 
 		sourcePath = new Path(
 				"/codegen-tools/target/generated-resources/jaxb-episode");
-		assertSourcePathFound(cp1, sourcePath);
+		assertSourcePathFound(cp, sourcePath);
 
 		String file = "target/generated-sources/jaxb-episode/org/ebayopensource/turmeric/runtime/codegen/common/CodeGenOptionType.java";
 		assertTrue(project.getFile(file).isSynchronized(IResource.DEPTH_ZERO));
@@ -74,20 +74,37 @@ public class TurmericStandardProjectConfiguratorTest extends
 	public void testTypeLibraryProject() throws Exception {
 		String testProjectPOMPath = "projects/common-type-library/pom.xml";
 		IProject project = buildProject(testProjectPOMPath);
-		IClasspathEntry[] cp1 = getClasspath(project);
+		IClasspathEntry[] cp = getClasspath(project);
 
 		Path sourcePath = new Path(
 				"/common-type-library/target/generated-sources/codegen");
-		assertSourcePathFound(cp1, sourcePath);
+		assertSourcePathFound(cp, sourcePath);
 
 		sourcePath = new Path(
 				"/common-type-library/target/generated-resources/codegen");
-		assertSourcePathFound(cp1, sourcePath);
+		assertSourcePathFound(cp, sourcePath);
 
 		String file = "target/generated-sources/codegen/org/ebayopensource/turmeric/common/v1/types/AckValue.java";
 		assertTrue("File is not synchronized:  " + file, project.getFile(file)
 				.isSynchronized(IResource.DEPTH_ZERO));
 		assertTrue("File is not accessible: " + file, project.getFile(file).isAccessible());
+	}
+	
+	@Test
+	public void testErrorLibraryProject() throws Exception {
+		String testProjectPOMPath = "projects/runtime-error-library/pom.xml";
+		IProject project = buildProject(testProjectPOMPath);
+		IClasspathEntry[] cp = getClasspath(project);
+		
+		Path sourcePath = new Path("/runtime-error-library/target/generated-sources/codegen");
+		assertSourcePathFound(cp, sourcePath);
+		
+		sourcePath = new Path("/runtime-error-library/target/generated-resources/codegen");
+		assertSourcePathFound(cp, sourcePath);
+		
+		String file = "target/generated-sources/codegen/org/ebayopensource/turmeric/runtime/errorlibrary/ErrorConstants.java";
+		assertTrue("File is not synchronized: " + file, project.getFile(file).isSynchronized(IResource.DEPTH_ZERO));
+		assertTrue("File is not accessible:" + file, project.getFile(file).isAccessible());
 	}
 
 }
